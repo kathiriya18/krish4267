1e13312e38f863d5440e463913e075ec49f75b7e
kathiriya18 <131762299+kathiriya18@users.noreply.github.com>
2023-05-03 15:44:20 +0530

    Add files via upload

---
+++ krish4267.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+void main()
+{
+	int a=12,b=18;
+	
+	(a>b)? printf("a is maximum") :printf("b is maximum");
+}
---
+++ krish4268.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void main()
+{
+	int a=12,b=14,c=18;
+	
+	if(a,b)
+	{
+		if(a,c)
+		{
+			printf("a is mainimum");
+		
+		}
+		else
+		{
+			printf("c is minimum");
+		}
+	}
+	else
+	{
+		if(b<c)
+		{
+			printf("b is minimum");
+		}
+		else
+		{
+			printf("c is minimum");
+		}
+	}
+		
+		
+}
+
